987bd123e5b0752be81e6d1d86e7ac63b778c98d
Julio Avila <javila1997@gmail.com>
2020-01-01 17:31:24 -0300

    first commit

---
+++ src/pages/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//Styles
+import './home.scss';
+
+class Home extends React.Component {
+  render() {
+    return <h1>Home page works!</h1>;
+  }
+}
+
+export default Home;
